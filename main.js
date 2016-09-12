95d155ca012a45e0a384de6f52c10816d396313d
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-09-12 01:49:19 +0100

    Added console.log and saw it has updated

--- main.js
+++ main.js
@@ -9,3 +9,6 @@ entity.setAttribute('position', {x: 1, y: 2, z: 3});
 //testEntity.setAttribute('position', '1 2 3');
 // And:
 //testEntity.setAttribute('position', new THREE.Vector3(1, 2, 3);
+
+// Ah... But even though it doesn't show in the DOM, it is apparently set OK:
+console.log('position', entity.object3D.position);
