0bf9bf71c4390afd8424b115c33a9a89ef69f57c
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-09-12 08:49:37 +0100

    Add example of wrapping in a div

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var entity = document.getElementById('test-entity');
+var entity1 = document.getElementById('test-entity');
 
 // Try setting the position...
-entity.setAttribute('position', {x: 1, y: 2, z: -3});
+entity1.setAttribute('position', {x: -2, y: 2, z: -3});
 
 // ... But inspecting the DOM, it doesn't appear to be set.
 
@@ -11,12 +11,33 @@ entity.setAttribute('position', {x: 1, y: 2, z: -3});
 //testEntity.setAttribute('position', new THREE.Vector3(1, 2, 3);
 
 // Ah... But even though it doesn't show in the DOM, it is apparently set OK:
-console.log('position', entity.object3D.position);
+console.log('position', entity1.object3D.position);
 
 // OK cool, let's try adding an image inside it then
-var image = document.createElement('a-image');
-image.setAttribute('src', 'test.jpg');
-image.setAttribute('width', 1);
-image.setAttribute('height', 1);
+var image1 = document.createElement('a-image');
+image1.setAttribute('src', 'walrus.jpg');
+image1.setAttribute('width', 1);
+image1.setAttribute('height', 1);
 
-entity.appendChild(image);
+entity1.appendChild(image1);
+
+// That worked! OK, now to try adding one inside a <div>
+
+var container = document.createElement('div');
+var entity2 = document.createElement('a-entity');
+
+entity2.setAttribute('position', {x: 2, y: 2, z: -3});
+
+var image2 = document.createElement('a-image');
+
+image2.setAttribute('src', 'xkcd.png');
+image2.setAttribute('width', 1);
+image2.setAttribute('height', 1);
+
+entity2.appendChild(image2);
+container.appendChild(entity2);
+
+var scene = document.querySelector('a-scene');
+scene.appendChild(container);
+
+// Yeah this one doesn't show up...
